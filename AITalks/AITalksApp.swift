51aba48dddbf57cd46f18eab94d95566ffc69dc5
Leonid117 <Leonid.v117@gmail.com>
2023-06-16 06:16:45 +0800

    Initial Commit

---
+++ AITalks/AITalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITalksApp.swift
+//  AITalks
+//
+//  Created by Leonid Volkov on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AITalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
